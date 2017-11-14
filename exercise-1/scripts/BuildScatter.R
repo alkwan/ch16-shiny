2407bf7cd83921e23775cecd9f32860c4c441a3c
alainakwan <kwan.alaina@gmail.com>
2017-11-14 13:54:04 -0800

    wrote scatterplot function

--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,15 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+
+MakeScatterPlot <- function(new.data, new.x, new.y, new.color, new.title, label.x, label.y) {
+  library(ggplot2)
+  if (length(new.title) < 1) {
+    title <- 'Title'
+  }
+  return(ggplot(data = new.data) +
+    geom_point(mapping = aes(x = new.x, y = new.y), color = new.color) +
+    labs(title = title,
+         x = label.x,
+         y = label.y))
+}
